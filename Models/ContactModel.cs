6db11f59e83fcf5e5ea7c234106e8037e684feb8
James Peart <jamespeart@jamess-mbp.lan>
2020-03-09 00:10:00 -0500

    pre-submit build

---
+++ Models/ContactModel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EcycleBlazor.Models
+{
+  public class ContactModel
+  {
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    [EmailAddress(ErrorMessage = "Please use a valid email address")]
+    public string Email { get; set; }
+    [Required]
+    [MinLength(3, ErrorMessage = "Your message must be atleast 3 characters long")]
+    public string Message { get; set; }
+  }
+}
+
